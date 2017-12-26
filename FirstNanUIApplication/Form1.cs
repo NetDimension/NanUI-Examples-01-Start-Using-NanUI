a1fca46a43e50859cfac3a009f4f80e09018bff1
林选臣 <xuanchenlin@msn.com>
2017-12-26 17:53:17 +0800

    添加项目文件。

---
+++ FirstNanUIApplication/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstNanUIApplication
+{
+	using NetDimension.NanUI;
+
+	public partial class Form1 : Formium
+	{
+		public Form1()
+			: base("http://cn.bing.com", false)
+			// 第一个参数使用Bing来作为初始的网址，
+			// 第二个参数强制NanUI使用原生的窗体样式来呈现界面
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ FirstNanUIApplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstNanUIApplication
+{
+	using NetDimension.NanUI;
+
+	static class Program
+	{
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			Bootstrap.BeforeCefInitialize = (CefInitArgs) => {
+				//禁用日志
+				CefInitArgs.Settings.LogSeverity = Chromium.CfxLogSeverity.Disable;
+
+				//指定中文为当前CEF环境的默认语言
+				CefInitArgs.Settings.AcceptLanguageList = "zh-CN";
+				CefInitArgs.Settings.Locale = "zh-CN";
+
+
+
+			};
+
+			Bootstrap.BeforeCefCommandLineProcessing = (CefCmdArgs) =>
+			{
+				//在启动参数中添加disable-web-security开关，禁用跨域安全检测
+				CefCmdArgs.CommandLine.AppendSwitch("disable-web-security");
+			};
+
+			//指定CEF架构和文件目录结构，并初始化CEF
+			if(Bootstrap.Load(PlatformArch.Auto, System.IO.Path.Combine(Application.StartupPath, "fx"), System.IO.Path.Combine(Application.StartupPath, "fx\\Resources"), System.IO.Path.Combine(Application.StartupPath, "fx\\Resources\\locales")))
+			{
+				Application.Run(new Form1());
+			}
+
+			
+		}
+	}
+}
